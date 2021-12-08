b6ade9a5612dfc74b4cc2213d5ef759c4af70524
MichaelTennyson <55544189+MichaelTennyson@users.noreply.github.com>
2021-12-08 17:29:54 +0000

    twelfth commit. Map chunk size editor has been added to the mapGenerator and map display scripts.Terrain texture pack has also beem added to the project

--- Assignment1/Assets/Scripts/MeshGenerator.cs
+++ Assignment1/Assets/Scripts/MeshGenerator.cs
@@ -4,28 +4,31 @@ using UnityEngine;
 
 public static class MeshGenerator
 {
-    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve)
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
     {
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
         float topLeftX = (width - 1) / -2f;
         float topLeftZ = (height - 1) / -2f;
 
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        int verticesPerLine = (width - 1) / meshSimplificationIncrement + 1;
+
         MeshData meshData = new MeshData(width, height);
         int vertexIndex = 0;
 
-        //for loop through height map to increment
-        for (int y = 0; y < height; y++)
+        //for loop through height map to increment, mesh Simplificationincrement set as the incrementation rule so that the loop finishes 
+        for (int y = 0; y < height; y += meshSimplificationIncrement)
         {
-            for (int x = 0;x < width; x++)
+            for (int x = 0;x < width; x += meshSimplificationIncrement)
             {
                 meshData.vertices [vertexIndex] = new Vector3(topLeftX + x, heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, topLeftZ - y);
                 meshData.uvs[vertexIndex] = new Vector2(x / (float)width, y / (float)height);
                 if (x < width -1 && y < height - 1)
                 {
                     //first triangle setup: point a, b and c are initialised
-                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width);
-                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + width);
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + width);
+                    meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + width);
 
                 }
 
--- Assignment1/Assets/Scripts/mapGenerator.cs
+++ Assignment1/Assets/Scripts/mapGenerator.cs
@@ -6,8 +6,10 @@ public class mapGenerator : MonoBehaviour
 {
     public enum DrawMode{NoiseMap, ColourMap, Mesh}
     public DrawMode drawMode;
-    public int mapWidth;// map width variable that sets the width of the map in unity
-    public int mapHeight;// map height variable that sets the width of the map in unity
+
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;
     public float noiseScale;
 
     public bool autoUpdate;
@@ -22,24 +24,24 @@ public class mapGenerator : MonoBehaviour
     public TerrainType[] regions;
     public float meshHeightMultiplier;
     public AnimationCurve meshHeightCurve;
+   
 
 
     public void generateMap()
     {
-       float[,] noiseMap = noise.GenerateNoiseMap(mapWidth, mapHeight, seed,  noiseScale, octaves, persistance, lacunarity, offset);
+        float[,] noiseMap = noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-
-        Color[] colourMap = new Color[mapWidth * mapHeight];
-        for(int y = 0; y < mapHeight; y++)
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+        for(int y = 0; y < mapChunkSize; y++)
         {
-            for (int x = 0; x < mapWidth; x++)
+            for (int x = 0; x < mapChunkSize; x++)
             {
                 float currentHeight = noiseMap[x, y];
                 for (int  i = 0; i < regions.Length; i++)
                 {
                     if (currentHeight <= regions[i].height)
                     {
-                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        colourMap[y * mapChunkSize + x] = regions[i].colour;
                         break; 
                     }
                 }
@@ -53,25 +55,17 @@ public class mapGenerator : MonoBehaviour
             display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
         }
         else if (drawMode == DrawMode.ColourMap){
-            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
         }
         else if(drawMode == DrawMode.Mesh)
         {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
         }
     }
 
 
     private void OnValidate()
     {
-        if (mapWidth < 1)
-        {
-            mapWidth = 1;
-        }
-        if(mapHeight < 1)
-        {
-            mapHeight = 1;
-        }
         if (lacunarity < 1)
         {
             lacunarity = 1;
